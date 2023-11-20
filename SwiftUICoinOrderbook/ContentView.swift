91483340f8b0b5655a7747b8bfc38e757d659871
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-11-20 11:46:34 +0900

    Initial Commit

---
+++ SwiftUICoinOrderbook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICoinOrderbook
+//
+//  Created by 김지연 on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICoinOrderbook/SwiftUICoinOrderbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoinOrderbookApp.swift
+//  SwiftUICoinOrderbook
+//
+//  Created by 김지연 on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoinOrderbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
