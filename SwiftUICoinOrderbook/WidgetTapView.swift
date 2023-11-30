7f6102ddf75a4684648c1db8ceacd08451b87472
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-11-30 17:24:07 +0900

    231130 강의 - SwiftUI Widget 화면 이동

--- SwiftUICoinOrderbook/WidgetTapView.swift
+++ SwiftUICoinOrderbook/WidgetTapView.swift
@@ -1,5 +1,5 @@
 //
-//  WidgetTapView.swift
+//  WidgetTabView.swift
 //  SwiftUICoinOrderbook
 //
 //  Created by 김지연 on 11/30/23.
@@ -10,13 +10,12 @@ import SwiftUI
 struct WidgetTapView: View {
     
     @State private var selectedTab = "book"
-    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             Button("1111", action: {
                 selectedTab = "star"
-            })
-                .tabItem { Image(systemName: "book") }
+                print("tap")
+            }).tabItem { Image(systemName: "book") }
                 .tag("book")
             Text("2")
                 .tabItem { Image(systemName: "star") }
@@ -25,13 +24,13 @@ struct WidgetTapView: View {
                 .tabItem { Image(systemName: "pencil") }
                 .tag("pencil")
         }
-        .onOpenURL {
-            switch $0.absoluteString {
+        .onOpenURL(perform: { url in
+            switch url.absoluteString {
                 case "History": selectedTab = "book"
                 case "Favorite": selectedTab = "star"
                 default: selectedTab = "pencil"
             }
-        }
+        })
     }
 }
 
